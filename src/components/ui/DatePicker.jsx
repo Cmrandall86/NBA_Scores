33c3584ae6a6604c07aad41a5faf116c11d73088
Cmrandall86 <cmrandall86@gmail.com>
2023-12-12 19:28:48 -0700

    defaulted selected day to today's date

--- src/components/ui/DatePicker.jsx
+++ src/components/ui/DatePicker.jsx
@@ -1,12 +1,13 @@
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import dayjs from 'dayjs';
 
 export default function Date_Picker({ setGameDay, gameDay }) {
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
       <DatePicker
-        // defaultValue={gameDay}
+        defaultValue={dayjs(gameDay)}
         onChange={setGameDay}
       />
     </LocalizationProvider>
